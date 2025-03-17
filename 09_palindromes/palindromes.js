0d2e4509c335aaeaef1d7185162912dfa0fc1df9
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-03-17 16:10:43 +0000

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+  // convert string to array?
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const palindromeString = palindrome
+    .toLowerCase()
+    .split("")
+    .filter((letter) => alphanumerical.includes(letter))
+    .join("");
+  // iterate over array from reverse to create new array
+  const reversedPalindromeString = palindromeString
+    .split("")
+    .reverse()
+    .join("");
+
+  // compare both arrays?
+  console.log("reversed " + reversedPalindromeString);
+  console.log("forward " + palindromeString);
+
+  return reversedPalindromeString === palindromeString;
 };
 
 // Do not edit below this line
