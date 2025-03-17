4110a6195545a4e82f468a5b7e4f9389bb9d0d36
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-03-17 17:32:37 +0000

    Find the oldest completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (personArray) {
+  //
+  const orderedPersonArray = personArray.sort((a, b) => {
+    let currentPersonAge;
+    let nextPersonAge;
+    const currentYear = new Date().getFullYear();
 
+    if (!a.yearOfDeath) {
+      currentPersonAge = currentYear - a.yearOfBirth;
+    } else {
+      currentPersonAge = a.yearOfDeath - a.yearOfBirth;
+    }
+
+    if (!b.yearOfDeath) {
+      nextPersonAge = currentYear - b.yearOfBirth;
+    } else {
+      nextPersonAge = b.yearOfDeath - b.yearOfBirth;
+    }
+
+    return currentPersonAge > nextPersonAge ? 1 : -1;
+  });
+
+  return orderedPersonArray[orderedPersonArray.length - 1];
 };
 
 // Do not edit below this line
