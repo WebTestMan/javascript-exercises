111167bbd4c0b4703ebb534d1e9624c4ebd9bf52
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-03-17 17:03:07 +0000

    Get Titles completed

--- 11_getTheTitles/getTheTitles.spec.js
+++ 11_getTheTitles/getTheTitles.spec.js
@@ -1,18 +1,25 @@
-const getTheTitles = require('./getTheTitles')
+const getTheTitles = require("./getTheTitles");
 
-describe('getTheTitles', () => {
-    const books = [
-      {
-        title: 'Book',
-        author: 'Name'
-      },
-      {
-        title: 'Book2',
-        author: 'Name2'
-      }
-    ]
+describe("getTheTitles", () => {
+  const books = [
+    {
+      title: "Book",
+      author: "Name",
+    },
+    {
+      title: "Book2",
+      author: "Name2",
+    },
+  ];
 
-  test('gets titles', () => {
-    expect(getTheTitles(books)).toEqual(['Book','Book2']);
+  test("gets titles", () => {
+    expect(getTheTitles(books)).toEqual(["Book", "Book2"]);
+  });
+
+  const books2 = [...books];
+  books2.push({ title: "Book3", author: "Name3" });
+
+  test("gets more titles", () => {
+    expect(getTheTitles(books2)).toEqual(["Book", "Book2", "Book3"]);
   });
 });
