a21d38bf8b07de464a5bf0ac10db7aca0e231a4b
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-03-17 16:49:33 +0000

    Fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (sequenceNumber) {
+  if (sequenceNumber < 0) {
+    return "OOPS";
+  }
 
+  if (!Number.isInteger(sequenceNumber)) {
+    sequenceNumber = Number(sequenceNumber);
+  }
+
+  if (sequenceNumber === 0) return 0;
+  if (sequenceNumber === 1 || sequenceNumber === 2) return 1;
+
+  const sequenceArray = [1, 1];
+
+  for (i = 2; i < sequenceNumber; i++) {
+    sequenceArray.push(sequenceArray[i - 1] + sequenceArray[i - 2]);
+  }
+
+  return sequenceArray[sequenceArray.length - 1];
 };
 
 // Do not edit below this line
