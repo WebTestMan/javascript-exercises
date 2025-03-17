372ab28235fe5f5d2d155230bd3f068f4faade2a
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-03-17 12:44:33 +0000

    completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  } else {
+    return num1 + num2;
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  } else {
+    return num1 - num2;
+  }
 };
 
-const sum = function() {
-	
+const sum = function (numArray) {
+  if (!Array.isArray(numArray)) {
+    return "ERROR";
+  } else {
+    const productOfAllNums = numArray.reduce((total, currentItem) => {
+      return total + currentItem;
+    }, 0);
+    return productOfAllNums;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (numArray) {
+  if (!Array.isArray(numArray)) {
+    return "ERROR";
+  } else {
+    return numArray.reduce((product, current) => product * current);
+  }
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  } else {
+    return Math.pow(num1, num2);
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function (num1) {
+  let product = 1;
+  for (let i = num1; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +56,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
